f4ac0ffa42dcd0ea1ecf1e1aee0485dee936e56d
staaleandrb <staale@ingsta.com>
2024-06-19 10:19:37 +0200

    endringer

--- API/eksempel/app.js
+++ API/eksempel/app.js
@@ -1,20 +1,13 @@
-function fetchWeather() {
-    const weatherDiv = document.getElementById("weather");
-    const apiUrl = "https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=60.10&lon=9.58";
-
-    fetch(apiUrl, {
-        headers: {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
-        }
-    })
-    .then(response => response.json())
-    .then(data => {
-        const temperature = data.properties.timeseries[5].data.instant.details.air_temperature;
-        weatherDiv.textContent = `Temperatur: ${temperature}°C`;
-    })
-    .catch(error => {
-        weatherDiv.textContent = `Det oppstod en feil: ${error.message}`;
+document.addEventListener('DOMContentLoaded', function() {
+    const apiUrl = "https://speedskatingresults.com/api/json/world_records.php";
+    const params = new URLSearchParams({
+        gender: 'f', // Begrens til kvinner
+        distance: '500' // Spesifiser avstanden til 500 meter
     });
-}
 
-fetchWeather();
+    // Kombiner API-URL med parameterne
+    const apiUrlWithParams = `${apiUrl}?${params.toString()}`;
+    
+    fetch(apiUrlWithParams)
+        response => response.json()
+        console.log(response);
